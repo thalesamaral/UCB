ec47b5d16456734381b18fab687f0c293e3a3499
Thales <thaleslima225@gmail.com>
2023-04-07 05:05:43 -0300

    inicio ex3

---
+++ 03-Estrutura_de_Dados/Lista1/lista01ex03.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#define TAM 5
+
+int main(){
+// Declarações
+	char nome[TAM], nomeRef[TAM];
+	int i;
+	
+// Principal
+	//printf("");
+	//scanf("%d",&);
+
+    printf("Digite o Nome completo: ");
+    gets(nome);
+    //scanf("%s",&nome[i]);
+    printf("%s",nome);
+
+    /*for (i=0; i<TAM; i++){
+        
+    }*/
+
+}
