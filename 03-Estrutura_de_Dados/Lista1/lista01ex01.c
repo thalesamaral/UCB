492cfbd76cdb692be277988c7e3d122be2456bd2
Thales <thaleslima225@gmail.com>
2023-04-08 23:18:34 -0300

    ex4 concluído

--- 03-Estrutura_de_Dados/Lista1/lista01ex01.c
+++ 03-Estrutura_de_Dados/Lista1/lista01ex01.c
@@ -1,55 +1,40 @@
 #include <stdio.h>
+#include <string.h>
 #define TAM 4
 
 int main(){
 // Declarações
 	int numero[TAM];
 	int par[TAM], impar[TAM];
-	int i=0, j=0, k=0, l=0;
+	int i, j=0, k=0;
 	
 // Principal
-	
-	//printf("Digite um numero: ");
-	/*for(i=0; i<TAM; i++){
-		printf("Digite um numero: ");
-		scanf("%d",&numero[i]);
-	}*/
 
+	//LEITURA
+	printf("** Digite %d Numeros **\n\n",TAM);
 	for(i=0; i<TAM; i++){
-		printf("Digite um numero: ");
+		printf("%do numero: ",i+1);
 		scanf("%d",&numero[i]);
+	}
 
+	//DISTRIBUIÇÃO DE PAR E IMPAR
+	for(i=0; i<TAM; i++){
 		if(numero[i]%2==0){
 			par[j++] = numero[i];
 		}else{
 			impar[k++] = numero[i];
 		}
-
-		if(i == TAM-1){ //quando acaba o FOR principal
-			for(l=j; l<TAM; l++){
-				par[l] = -1;
-			}
-			for(l=k; l<TAM; l++){
-				impar[l] = -1;
-			}
-		}
-	}
-	i=0;
-	for(i=0; par[i] != -1; i++){
-		printf("\nPAR: %d",par[i]);
 	}
 
-	for(i=0; impar[i] != -1; i++){
-		printf("\nIMPAR: %d",impar[i]);
-	}
+	printf("\n***********************\n\n");
 
-	/*for(i=0; i<TAM; i++){
-		printf("Vetor Par valor: %d\n",par[i]);
+	//APRESENTAÇÃO DE PAR E IMPAR
+	for(i=0; i<j; i++){
+		printf("Vetor Par: %d\n",par[i]);
 	}
-
-	for(i=0; i<TAM; i++){
-		printf("Vetor Impar valor: %d\n",impar[i]);
-	}*/
+	for(i=0; i<k; i++){
+		printf("Vetor Impar: %d\n",impar[i]);
+	}
 
 	return 0;
 }
