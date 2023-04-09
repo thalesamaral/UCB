8e0f84b5d6cb53c92e39c469a7da4465276a0dcf
Thales <thaleslima225@gmail.com>
2023-04-08 23:50:49 -0300

    ex5 base

---
+++ 03-Estrutura_de_Dados/Lista1/lista01ex05.c
@@ -0,0 +1,80 @@
+#include <stdio.h>
+#include <string.h>
+#define TAM 10
+
+void imprimirVetor(int[], int);
+void bubbleSort(int[], int);
+
+int main(){
+// Declarações
+	int numero[TAM] = {5,7,8,1,3,9,2,10,4,6};
+	int par[TAM], impar[TAM];
+	int i, j=0, k=0;
+	
+// Principal
+
+	//LEITURA
+	printf("** Digite %d Numeros **\n\n",TAM);
+	/*for(i=0; i<TAM; i++){
+		printf("%do numero: ",i+1);
+		scanf("%d",&numero[i]);
+	}*/
+
+	//DISTRIBUIÇÃO DE PAR E IMPAR
+	for(i=0; i<TAM; i++){
+		if(numero[i]%2==0){
+			par[j++] = numero[i];
+		}else{
+			impar[k++] = numero[i];
+		}
+	}
+
+	printf("\n***********************\n\n");
+
+	//APRESENTAÇÃO DE PAR E IMPAR
+	for(i=0; i<j; i++){
+		printf("Vetor Par: %d\n",par[i]);
+	}
+	for(i=0; i<k; i++){
+		printf("Vetor Impar: %d\n",impar[i]);
+	}
+
+    bubbleSort(numero, TAM);
+    imprimirVetor(numero, TAM);
+
+    bubbleSort(par, j);
+    imprimirVetor(par, j);
+
+    bubbleSort(impar, k);
+    imprimirVetor(impar, k);
+	return 0;
+}
+
+//*** Bubble Sort *****************************************
+void bubbleSort(int vet[], int n){
+    int bolha, borda, aux, compara=0, troca=0;
+	
+	for(borda=n-1; borda>0; borda--){
+		for(bolha=0; bolha<borda; bolha++){
+			compara++;
+			if(vet[bolha] > vet[bolha+1]){ 
+				aux = vet[bolha];
+				vet[bolha] = vet[bolha+1];
+				vet[bolha+1] = aux;
+				troca++;
+			}
+		}
+	}
+	printf("\nBubble Sort Comparacoes: %i\n",compara);
+	printf("Bubble Sort Troca: %i\n\n",troca);
+
+}
+
+//*** Imprime os valores do vetor *************************
+void imprimirVetor(int vet[], int n){
+	int i;
+
+	for(i=0; i<n; i++){
+		printf("vetor[%d] = %d\n", i, vet[i]);
+    }
+}
