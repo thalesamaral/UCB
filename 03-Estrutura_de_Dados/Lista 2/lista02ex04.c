afe0cc18e7e3a6c88b5ec8a5581fdb4a91dbfd99
Thales <thaleslima225@gmail.com>
2023-05-06 12:41:08 -0300

    Create lista02ex04.c

---
+++ 03-Estrutura_de_Dados/Lista 2/lista02ex04.c
@@ -0,0 +1,20 @@
+//  Sintese
+//  Nome....: "Thales Amaral Lima"
+//  Data....: "06/05/2023"
+/*	Objetivo: Elabore um programa utilizando Structs onde serão informados "nome", "notas"(entre 1 e 10) e turma (A1, B2, C3 ou D4) de até 10 alunos, ao final apresente os nomes dos alunos que obtiveram a maior nota, apresente a turma (ou turmas) dos estudantes que obtiveram a maior nota, apresente também a quantidade de alunos por turma com a maior nota identificada. */
+#include <stdio.h>
+#include <stdlib.h>
+#define TAM 10
+
+//=== BLOCO PRINCIPAL ===============================================
+int main(){
+// Declarações
+	int i=0;
+		
+// Principal
+	//printf("");
+
+
+	return 0;
+}
+//=== FIM DO BLOCO ==================================================
