3806124bcf9cfcb63c29b0b90a86e4b786bd2a13
Thales <thaleslima225@gmail.com>
2023-05-07 02:01:28 -0300

    Create lista02ex04.2.c

---
+++ 03-Estrutura_de_Dados/Lista 2/lista02ex04.2.c
@@ -0,0 +1,40 @@
+//  Sintese
+//  Nome....: "Thales Amaral Lima"
+//  Data....: "07/05/2023"
+/*	Objetivo: Elabore um programa onde o usuário armazenará por meio de Structs os dados de "Produto" e "Fabricante", deverão serem cadastrados no mínimo 2 fabricantes (máximo 5) e no mínimo 5 produtos (máximo 50). */
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#define MAXP 50
+#define MAXF 5
+
+struct tProduto{
+	char descricao[50];
+	float peso;
+	float valorCompra;
+	float valorVenda;
+	float valorLucro;
+	float porcentoLucro;
+	struct tFabricante fabricante;
+};
+struct tFabricante{
+	char marca[50];
+	char site[50];
+	int telefone;
+	char uf[2];
+};
+
+//=== BLOCO PRINCIPAL ===============================================
+int main(){
+// Declarações
+	int i=0;
+	struct tProduto produto[MAXP];
+	//struct tFabricante
+
+// Principal
+
+	
+
+	return 0;
+}
+//=== FIM DO BLOCO ==================================================
