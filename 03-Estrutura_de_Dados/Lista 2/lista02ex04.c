dbae9768fe50f560905604cb56ed20a7fd77ce45
Thales <thaleslima225@gmail.com>
2023-05-07 01:35:51 -0300

    ex4 Finalizado

--- 03-Estrutura_de_Dados/Lista 2/lista02ex04.c
+++ 03-Estrutura_de_Dados/Lista 2/lista02ex04.c
@@ -6,7 +6,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <locale.h> //Idioma
-#define TAM 3
+#define TAM 10
 
 int valida_nota(int);
 int valida_turma(char);
@@ -22,11 +22,10 @@ int main(){
 	setlocale(LC_ALL,"Portuguese");
 // Declarações
 	int i=0;
+	int maior, qtdA1=0, qtdB2=0, qtdC3=0, qtdD4=0;
 	struct tAluno aluno[TAM];
 
 // Principal
-	//printf("");
-
 	do{
 		printf("\n****** Nomes ******\n");
 		printf("Nome do aluno %d: ",i+1);
@@ -53,6 +52,37 @@ int main(){
 		i++;
 	}while (i < TAM);
 	
+	//verifica Maior nota -------------------------------------------
+	for(i=0;i<TAM;i++){
+		if(i == 0){
+			maior = aluno[i].nota;
+		}else if(aluno[i].nota > maior){
+					maior = aluno[i].nota;
+				}
+	}
+	printf("\n******************************\n");
+	printf("Maior Nota: %d\n",maior);
+	//Aluno(s) e Turma(s) que teve a maior nota ---------------------
+	for(i=0;i<TAM;i++){
+		if(aluno[i].nota == maior){
+			printf("\nAluno: %s",aluno[i].nome);
+			printf("\nTurma: %s",aluno[i].turma);
+			//Quantidade de alunos com maior nota -------------------
+			if(strcmp(aluno[i].turma,"A1")==0){
+				qtdA1++;
+			}else if(strcmp(aluno[i].turma,"B2")==0){
+				qtdB2++;
+			}else if(strcmp(aluno[i].turma,"C3")==0){
+				qtdC3++;
+			}else if(strcmp(aluno[i].turma,"D4")==0){
+				qtdD4++;
+			}
+		}
+	}
+	printf("\n\nQuantidade Turma A1: %d",qtdA1);
+	printf("\nQuantidade Turma B2: %d",qtdB2);
+	printf("\nQuantidade Turma C3: %d",qtdC3);
+	printf("\nQuantidade Turma D4: %d\n",qtdD4);
 	return 0;
 }
 //=== FIM DO BLOCO ==================================================
