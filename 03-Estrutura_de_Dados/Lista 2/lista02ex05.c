1368a0d36aa9e890a176ca7260c43d978ac04e48
Thales <thaleslima225@gmail.com>
2023-05-13 02:01:35 -0300

    ex5 e 6. ADD

---
+++ 03-Estrutura_de_Dados/Lista 2/lista02ex05.c
@@ -0,0 +1,21 @@
+//  Sintese
+//  Nome....: "Thales Amaral Lima"
+//  Data....: "13/05/2023"
+/*	Objetivo: Implemente um programa exemplo (livre) aplicando o uso de ponteiros.*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+//=== BLOCO PRINCIPAL ===============================================
+int main(void){
+//Declarações
+	
+//Instruções
+	//printf("");
+	//scanf("%",&);
+	
+	
+	
+	return 0;
+}
+//=== FIM DO BLOCO ==================================================
---
+++ 03-Estrutura_de_Dados/Lista 2/lista02ex06.c
@@ -0,0 +1,21 @@
+//  Sintese
+//  Nome....: "Thales Amaral Lima"
+//  Data....: "13/05/2023"
+/*	Objetivo: Elabore programa exemplo (livre) de passagem de parâmetros por valor e por referência, explique o funcionamento do código comentando linha a linha.*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+//=== BLOCO PRINCIPAL ===============================================
+int main(void){
+//Declarações
+	
+//Instruções
+	//printf("");
+	//scanf("%",&);
+	
+	
+	
+	return 0;
+}
+//=== FIM DO BLOCO ==================================================
