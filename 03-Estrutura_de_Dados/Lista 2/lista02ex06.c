3290794f70f0c8714c97895b4163879cb8270c0e
Thales <thaleslima225@gmail.com>
2023-05-15 19:47:18 -0300

    list2ex6. FINALIZADO

--- 03-Estrutura_de_Dados/Lista 2/lista02ex06.c
+++ 03-Estrutura_de_Dados/Lista 2/lista02ex06.c
@@ -1,21 +1,41 @@
 //  Sintese
 //  Nome....: "Thales Amaral Lima"
-//  Data....: "13/05/2023"
+//  Data....: "15/05/2023"
 /*	Objetivo: Elabore programa exemplo (livre) de passagem de parâmetros por valor e por referência, explique o funcionamento do código comentando linha a linha.*/
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+// Protótipo da função **********************************************
+void nomeFuncao(int, int*); //Por REFERÊNCIA deve adicionar um asterisco para o tipo da variável
 
 //=== BLOCO PRINCIPAL ===============================================
 int main(void){
 //Declarações
-	
+	int valor = 0, referencia = 0; //valores iniciados com 0
 //Instruções
-	//printf("");
-	//scanf("%",&);
 	
+	printf("\n*** Valores no MAIN (Antes de passar na Funcao) ****\n");
+	printf("Tipo Valor.....: %d\n",valor);
+	printf("Tipo Referencia: %d\n",referencia);
 	
+	nomeFuncao(valor, &referencia); //Por REFERÊNCIA deve adicionar um "E" Comercial para a variável
+	
+	printf("\n*** Valores no MAIN (depois de passar na Funcao) ***\n");
+	printf("Tipo Valor.....: %d\n",valor); //no MAIN o tipo valor não é alterado pela função
+	printf("Tipo Referencia: %d\n",referencia); //no MAIN o tipo referência é alterado pela função
 	
 	return 0;
 }
 //=== FIM DO BLOCO ==================================================
+
+// Função
+void nomeFuncao(int tipoValor, int *tipoReferencia){ //Por REFERÊNCIA deve adicionar um asterisco para a variável
+	int coisa = 1; // coisa = variável; valor; expressão; Função.
+
+	tipoValor = coisa; // exemplo de uso do parâmetro POR VALOR
+	*tipoReferencia = coisa; // exemplo de uso do parâmetro POR REFERÊNCIA
+
+	printf("\n*** Valores na Funcao ******************************\n");
+	printf("Tipo Valor.....: %d\n",tipoValor); //dentro da função o tipoValor apresenta o novo dado
+	printf("Tipo Referencia: %d\n",*tipoReferencia); //dentro da função o tipoReferencia apresenta o novo dado e altera o dado no MAIN da variável referenciada
+}
