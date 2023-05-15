151e4ed07d64e00375d68626f0cd5c70b3a020e3
Thales <thaleslima225@gmail.com>
2023-05-15 20:28:03 -0300

    list2ex5. FINALIZADO

--- 03-Estrutura_de_Dados/Lista 2/lista02ex05.c
+++ 03-Estrutura_de_Dados/Lista 2/lista02ex05.c
@@ -1,21 +1,30 @@
 //  Sintese
 //  Nome....: "Thales Amaral Lima"
-//  Data....: "13/05/2023"
+//  Data....: "15/05/2023"
 /*	Objetivo: Implemente um programa exemplo (livre) aplicando o uso de ponteiros.*/
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 
 //=== BLOCO PRINCIPAL ===============================================
 int main(void){
 //Declarações
-	
+	int x;
+	int *y; //Arsterisco indica que é um ponteiro, pra int.
 //Instruções
-	//printf("");
-	//scanf("%",&);
+	//printf("");	
 	
+	//#1: x recebe 5 em seu endereço de memória
+	x = 5;
+	printf("X = %d\n",x);
 	
+	//#2: y aponta para o endereço de memória de x
+	//E Comercial indica o endereço de memória.
+	y = &x;
 	
+	//#3: onde o y aponta, que é o endereço de x, recebe 3.
+	*y = 3;
+	printf("X = %d\n",x);
+
 	return 0;
 }
 //=== FIM DO BLOCO ==================================================
