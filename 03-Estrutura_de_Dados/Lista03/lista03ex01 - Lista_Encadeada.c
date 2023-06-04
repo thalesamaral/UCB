12ea3ba236cef1cc83b15a9572df6234931504ce
Thales <thaleslima225@gmail.com>
2023-06-04 15:35:33 -0300

    create exercicios

---
+++ 03-Estrutura_de_Dados/Lista03/lista03ex01 - Lista_Encadeada.c
@@ -0,0 +1,21 @@
+//  Sintese
+//  Nome....: "Thales Amaral Lima"
+//  Data....: "00/00/2023"
+/*	Objetivo: */
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+//=== BLOCO PRINCIPAL ===============================================
+int main(void){
+//Declarações
+	
+//Instruções
+	//printf("");
+	//scanf("%",&);
+	
+	
+	
+	return 0;
+}
+//=== FIM DO BLOCO ==================================================
