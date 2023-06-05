56e27c1978f41e77cde1209e30383ec84ca9cf81
Thales <thaleslima225@gmail.com>
2023-06-05 00:49:40 -0300

    create list3ex1

--- 03-Estrutura_de_Dados/Lista03/lista03ex01 - Lista_Encadeada.c
+++ 03-Estrutura_de_Dados/Lista03/lista03ex01 - Lista_Encadeada.c
@@ -6,16 +6,61 @@
 #include <stdlib.h>
 #include <string.h>
 
+//*** Declaracoes de tipos ******************************************
+struct tLista{
+    int numero;
+};
+
+struct tNo{
+    struct tLista dado;
+    struct tNo *prox; //ponteiro que aponta para o mesma tipo de estrutura
+};
+
+//*** Prototipos de funcoes *****************************************
+int menu(void);
+
 //=== BLOCO PRINCIPAL ===============================================
 int main(void){
 //Declarações
-	
+	struct tNo *lista=NULL; //inicialmente vazio
+    struct tNo *novo, *p; //ponteiro auxiliar
+    int opcao;
 //Instruções
-	//printf("");
-	//scanf("%",&);
-	
-	
-	
+    
+    do{
+        opcao = menu();
+        switch (opcao) {
+            case 1:
+                printf("\n*** Inclusao ***\n");
+                novo = malloc(sizeof(struct tNo)); //criar um no
+                printf("Digite um numero: ");
+                scanf("%d",&(novo->dado.numero));
+                novo->prox = lista;
+                lista = novo;
+            break;
+
+            case 2:
+                printf("\n*** Listagem ***\n");
+                p = lista;
+                while (p != NULL) {
+                    printf("numero: %d\n", p->dado.numero);
+                    p = p->prox; // x = x + 1
+                }
+            break;
+        }
+    }while (opcao != 0);
 	return 0;
 }
 //=== FIM DO BLOCO ==================================================
+
+//*** Menu **********************************************************
+int menu(void) {
+    int op;
+    printf("\n\n*** MENU ***\n\n");
+    printf("1. Inclusao\n");
+    printf("2. Listagem\n");
+    printf("0. Sair\n\n");
+    printf("Escolha sua opcao: ");
+    scanf("%d", &op);
+    return op;
+}
