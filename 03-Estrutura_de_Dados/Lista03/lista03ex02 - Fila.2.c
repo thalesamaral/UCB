17620b6159b533068a34ecc4c4f15fdebf8edb90
Thales <thaleslima225@gmail.com>
2023-06-10 01:05:55 -0300

    fila.2.1

--- 03-Estrutura_de_Dados/Lista03/lista03ex02 - Fila.2.c
+++ 03-Estrutura_de_Dados/Lista03/lista03ex02 - Fila.2.c
@@ -22,7 +22,6 @@ struct tFila{
 };
 
 //*** Prototipos de funcoes *****************************************
-int menu(void);
 void cria_fila(struct tFila*);
 int tamanho_fila(struct tFila*);
 void fila_vazia(struct tFila*);
@@ -32,88 +31,51 @@ int remove_elemento(struct tFila*);
 int main(void){
 //Declarações
 	struct tFila *fila = malloc(sizeof(struct tFila));
-	struct tNo *lista=NULL, *novo, *p;
-	int opcao;
 //Instruções
+	//INICIO --------------------------------------------------------
 	cria_fila(fila);
+	printf("Tamanho da lista: %d\n", tamanho_fila(fila));
+	fila_vazia(fila);
 
-	do{
-        opcao = menu();
-        switch (opcao) {
-            case 1:
-                printf("\n*** Enfileirar ***\n");
-                novo = malloc(sizeof(struct tNo));
-                printf("Digite um numero: ");
-                scanf("%d",&(novo->info.numero));
-                novo->prox = lista;
-                lista = novo;
-            break;
-
-            case 2:
-                printf("\n*** Desenfilerar ***\n");
-                p = dequeue(&fila);
-				if(p != NULL){
-					printf("Desenfilerou: %d\n", p->info.numero);
-					free(p);
-				}else{
-					printf("Fila vazia!\n");
-				} 
-            break;
-        }
-    }while (opcao != 0);
-	
+	//ELEMENTO 1 ----------------------------------------------------
+	struct tLista elemento1;
+	printf("\nInsira o numero: ");
+	scanf("%d", &elemento1.numero);
+	if(insere_elemento(fila, elemento1)){
+		printf("Elemento inserido com sucesso!\n\n");
+	}
 	printf("Tamanho da lista: %d\n", tamanho_fila(fila));
 	
 	fila_vazia(fila);
-	
-	struct tLista item1;
-	
+	//ELEMENTO 2 ----------------------------------------------------
+	struct tLista elemento2;
 	printf("\nInsira o numero: ");
-	scanf("%d", &item1.numero);
-	
-	if(insere_elemento(fila, item1)){
-		printf("Elemento inserido com sucesso!\n");
+	scanf("%d", &elemento2.numero);
+	if(insere_elemento(fila, elemento2)){
+		printf("Elemento inserido com sucesso!\n\n");
 	}
-	
-	printf("\nTamanho da lista: %d\n", tamanho_fila(fila));
+	printf("Tamanho da lista: %d\n", tamanho_fila(fila));
 	
 	fila_vazia(fila);
-	/*struct tLista item2;
-	
-	printf("\nInsira o numero: ");
-	scanf("%d", &item2.numero);
-	
-	if(insere_elemento(fila, item2)){
-		printf("Elemento inserido com sucesso!\n");
-	}
-	printf("Tamanho da lista: %d\n", tamanho_fila(fila));*/
-	
-	printf("\n------------------\n");
-	printf("Removendo um elemento!!\n");
-	printf("------------------\n\n");
-
+	//REMOVER ELEMENTO 2 --------------------------------------------
+	printf("\nRemovendo um elemento!!\n\n");
 	if(remove_elemento(fila)){
 		printf("Elemento removido com sucesso!\n");
 	}
+
+	fila_vazia(fila);
+	//REMOVER ELEMENTO 1 --------------------------------------------
+	printf("\n*** Removendo um elemento!! ***\n\n");
+	if(remove_elemento(fila)){
+		printf("Elemento removido com sucesso!\n\n");
+	}
 	
+	//FINAL --------------------------------------------------------
 	printf("Tamanho da lista: %d\n", tamanho_fila(fila));
 	fila_vazia(fila);
 	return 0;
 }
 //=== FIM DO BLOCO ==================================================
-
-//*** Menu **********************************************************
-int menu(void) {
-    int op;
-    printf("\n\n*** MENU ***\n\n");
-    printf("1. Enfileirar\n");
-	printf("2. Desenfilerar\n");
-    printf("3. Listagem\n");
-    printf("0. Sair\n\n");
-    printf("Escolha sua opcao: ");
-    scanf("%d", &op);
-    return op;
-}
 
 //*** Inicializar Fila **********************************************
 void cria_fila(struct tFila *fi){
