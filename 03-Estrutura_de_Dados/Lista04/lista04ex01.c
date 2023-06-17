9408cb8ac5d161d512800b1cca5907185a32dacb
Thales <thaleslima225@gmail.com>
2023-06-17 01:10:04 -0300

    validaUF em andamento

--- 03-Estrutura_de_Dados/Lista04/lista04ex01.c
+++ 03-Estrutura_de_Dados/Lista04/lista04ex01.c
@@ -18,19 +18,19 @@
 //*** Declaracoes de tipos ******************************************
 typedef struct {
     int codigo;
-	/*char marca[50];
-	char site[50];
-	int telefone;
-	char uf[2];*/
+	//char marca[50];
+	//char site[50];
+	//int telefone;
+	char uf[25];
 }tFabricante;
 
 struct tProduto{
 	char descricao[50];
+	//float peso;
 	float valorCompra;
-	/*float peso;
-	float valorVenda;
-	float valorLucro;
-	float porcentoLucro;*/
+	//float valorVenda;
+	//float valorLucro;
+	//float porcentoLucro;
 	tFabricante fabricanteFK;
 };
 
@@ -41,7 +41,7 @@ struct tCliente{
 
 //*** Prototipos de funcoes *****************************************
 int menu(void);
-//int validaUF(tFabricante);
+int validaUF(tFabricante*,int);
 void listarProduto(struct tProduto);
 void ordenarProdutos(struct tProduto*, int);
 void buscaBinaria(struct tProduto*, int, float);
@@ -99,12 +99,12 @@ int main(void){
 							printf("  Site: ");
 							gets(fabricante[qtdF].site);
 							printf("  Telefone: ");
-							scanf("%d", &fabricante[qtdF].telefone);
+							scanf("%d", &fabricante[qtdF].telefone);*/
 							do{
 								printf("  UF: ");
 								fflush(stdin);
 								gets(fabricante[qtdF].uf);
-							}while(validaUF(fabricante[qtdF]));*/
+							}while(validaUF(fabricante,qtdF));
 							qtdF++;
 						}else{
 								printf("Vetor cheio!\n");
@@ -314,9 +314,9 @@ int menu(void) {
     int op;
 
 		printf("\n\n*** MENU ***\n\n");
-		printf("1. Inclusao Fabricante\n");
-		printf("2. Inclusao Produto\n");
-        printf("3. Inclusao Cliente\n");
+		printf("1. INCLUSAO FABRICANTE\n");
+		printf("2. INCLUSAO PRODUTO\n");
+        printf("3. INCLUSAO CLIENTE\n");
 		printf("4. Listagem Produtos\n");
 		printf("5. Listagem Produtos de um Fabricante. Ordem alfabetica\n");
 		printf("6. Estados dos Produtos mais caro\n");//b
@@ -333,9 +333,28 @@ int menu(void) {
 		return op;
 }
 
+// Listagem geral dos Produtos **************************************
+void listarProduto(struct tProduto p){
+	printf("\n****************************************\n");
+	printf("*** PRODUTO\n");
+	printf(" Descricao: %s\n", p.descricao);
+	//printf(" Peso: %.2f\n", p.peso);
+	printf(" Valor Compra: %.2f\n", p.valorCompra);
+	//printf(" Valor Venda: %.2f\n", p.valorVenda);
+	//printf(" Valor Lucro: %.2f\n", p.valorLucro);
+	//printf(" Porcento Lucro: %.2f%%\n", p.porcentoLucro);
+	printf("--------------------\n");
+	printf("--- FABRICANTE\n");
+    printf(" Codigo: %d\n", p.fabricanteFK.codigo);
+	//printf(" Marca: %s\n", p.fabricanteFK.marca);
+	//printf(" Site: %s\n", p.fabricanteFK.site);
+	//printf(" Telefone: %d\n", p.fabricanteFK.telefone);
+	printf(" UF: %s\n", p.fabricanteFK.uf);
+}
+
 // Validar Estado (UF) **********************************************
-/*int validaUF(tFabricante f){
-	if((strcmp(f.uf,"RO")!=0) && (strcmp(f.uf,"AC")!=0) && (strcmp(f.uf,"AM")!=0) && (strcmp(f.uf,"RR")!=0)
+int validaUF(tFabricante *f, int i){
+	/*if((strcmp(f.uf,"RO")!=0) && (strcmp(f.uf,"AC")!=0) && (strcmp(f.uf,"AM")!=0) && (strcmp(f.uf,"RR")!=0)
 	&& (strcmp(f.uf,"PA")!=0) && (strcmp(f.uf,"AP")!=0) && (strcmp(f.uf,"TO")!=0) && (strcmp(f.uf,"MA")!=0)
 	&& (strcmp(f.uf,"PI")!=0) && (strcmp(f.uf,"CE")!=0) && (strcmp(f.uf,"RN")!=0) && (strcmp(f.uf,"PB")!=0)
 	&& (strcmp(f.uf,"PE")!=0) && (strcmp(f.uf,"AL")!=0) && (strcmp(f.uf,"SE")!=0) && (strcmp(f.uf,"BA")!=0)
@@ -346,29 +365,24 @@ int menu(void) {
 		printf("ERRO: digite um UF existente!!!\n\n");
 		return 1;
 	}else
+		return 0;*/
+	if((strcmp(f[i].uf,"RO")==0)){
+		strcat(f[i].uf,", Roraima");
 		return 0;
-}*/
-
-// Listagem geral dos Produtos **************************************
-void listarProduto(struct tProduto p){
-	printf("\n****************************************\n");
-	printf("*** PRODUTO\n");
-	printf(" Descricao: %s\n", p.descricao);
-	printf(" Valor Compra: %.2f\n", p.valorCompra);
-	/*printf(" Peso: %.2f\n", p.peso);
-	printf(" Valor Venda: %.2f\n", p.valorVenda);
-	printf(" Valor Lucro: %.2f\n", p.valorLucro);
-	printf(" Porcento Lucro: %.2f%%\n", p.porcentoLucro);*/
-	printf("--------------------\n");
-	printf("--- FABRICANTE\n");
-    printf(" Codigo: %d\n", p.fabricanteFK.codigo);
-	/*printf(" Marca: %s\n", p.fabricanteFK.marca);
-	printf(" Site: %s\n", p.fabricanteFK.site);
-	printf(" Telefone: %d\n", p.fabricanteFK.telefone);
-	printf(" UF: %s\n", p.fabricanteFK.uf);*/
+	}else if((strcmp(f[i].uf,"AC")==0)){
+		strcat(f[i].uf,", Acre");
+		return 0;
+	}
+	/*&& (strcmp(f.uf,"AC")!=0) && (strcmp(f.uf,"AM")!=0) && (strcmp(f.uf,"RR")!=0)
+	&& (strcmp(f.uf,"PA")!=0) && (strcmp(f.uf,"AP")!=0) && (strcmp(f.uf,"TO")!=0) && (strcmp(f.uf,"MA")!=0)
+	&& (strcmp(f.uf,"PI")!=0) && (strcmp(f.uf,"CE")!=0) && (strcmp(f.uf,"RN")!=0) && (strcmp(f.uf,"PB")!=0)
+	&& (strcmp(f.uf,"PE")!=0) && (strcmp(f.uf,"AL")!=0) && (strcmp(f.uf,"SE")!=0) && (strcmp(f.uf,"BA")!=0)
+	&& (strcmp(f.uf,"MG")!=0) && (strcmp(f.uf,"ES")!=0) && (strcmp(f.uf,"RJ")!=0) && (strcmp(f.uf,"SP")!=0)
+	&& (strcmp(f.uf,"PR")!=0) && (strcmp(f.uf,"SC")!=0) && (strcmp(f.uf,"RS")!=0) && (strcmp(f.uf,"MS")!=0)
+	&& (strcmp(f.uf,"MT")!=0) && (strcmp(f.uf,"GO")!=0) && (strcmp(f.uf,"DF")!=0))*/
 }
 
-// Função para ordenar um array de produtos em ordem alfabética pelo nome usando Bubble Sort
+// Ordenar produtos em ordem alfabética - Bubble Sort ***************
 void ordenarProdutos(struct tProduto* produtos, int tam){
     int trocado;
     struct tProduto aux;
